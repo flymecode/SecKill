5f78e827e6bb3ab78fdbb9fff95f7d32482a5445
马旭 <1272193309@qq.com>
2019-01-28 09:10:01 +0800

    数据抽取

--- src/main/java/com/miaoshaproject/until/SysConstant.java
+++ src/main/java/com/miaoshaproject/until/SysConstant.java
@@ -10,7 +10,13 @@ package com.miaoshaproject.until;
  * @author maxu
  */
 public class SysConstant {
-	public static String IS_LOGIN = "IS_LOGIN";
-	public static String LOGIN_USER = "LOGIN_USER";
+	public static final String IS_LOGIN = "IS_LOGIN";
+	public static final String LOGIN_USER = "LOGIN_USER";
+
+	public static final String TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+	public static final Integer PROMO_NO_START = 1;
+	public static final Integer PROMO_STARTING = 2;
+	public static final Integer PROMO_END = 3;
+	public static final Integer PROMO_NO = 0;
 
 }
