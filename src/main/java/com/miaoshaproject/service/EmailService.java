538ae42a1f3956f6c9dae850ae41938d96c58619
马旭 <1272193309@qq.com>
2019-01-28 13:23:34 +0800

    邮件初步完成

--- src/main/java/com/miaoshaproject/service/EmailService.java
+++ src/main/java/com/miaoshaproject/service/EmailService.java
@@ -1,20 +1,26 @@
 package com.miaoshaproject.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 /**
  * @author maxu
  * @date 2019/1/28
  */
 @Service
+@Slf4j
 public class EmailService {
 
     @Value("${spring.mail.username}")
@@ -22,6 +28,17 @@ public class EmailService {
 
     @Autowired
     private JavaMailSender javaMailSender;
+
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    public void s() throws MessagingException {
+        Context context = new Context();
+        context.setVariable("id","006");
+        String emailContent = templateEngine.process("emailTemplate", context);
+        sendHtmlMail("","",emailContent);
+    }
 
     public void sendSimpleMail(String to, String subject, String content) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
@@ -40,5 +57,39 @@ public class EmailService {
         helper.setFrom(from);
         helper.setText(content,true);
         javaMailSender.send(mimeMessage);
+    }
+
+    public void sendAttachmentMail(String to, String subject, String content,String filePath) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,true);
+        helper.setTo(to);
+        helper.setFrom(from);
+        helper.setText(content,true);
+        FileSystemResource file = new FileSystemResource(new File(filePath));
+        String fileName = file.getFilename();
+        helper.addAttachment(fileName,file);
+        helper.addAttachment(fileName+"_test",file);
+        helper.addAttachment(fileName+"_test1",file);
+        javaMailSender.send(mimeMessage);
+    }
+
+    public void sendInlineResourceMail(String to, String subject, String content,String rscPath,String rscId) {
+        log.info("发送邮件参数：{},{},{},{},{}", to, subject, content, rscPath, rscId);
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = null;
+        try {
+            helper = new MimeMessageHelper(mimeMessage,true);
+            helper.setTo(to);
+            helper.setFrom(from);
+            helper.setText(content,true);
+            FileSystemResource res = new FileSystemResource(new File(rscPath));
+            helper.addInline(rscId,res);
+            javaMailSender.send(mimeMessage);
+            log.info("发送成功");
+        } catch (MessagingException e) {
+            log.error("发送失败",e);
+        }
+
+
     }
 }
--- src/main/java/com/miaoshaproject/until/SysConstant.java
+++ src/main/java/com/miaoshaproject/until/SysConstant.java
@@ -7,13 +7,14 @@ package com.miaoshaproject.until;
 
 /**
  * 常量值
+ *
  * @author maxu
  */
 public class SysConstant {
 	public static final String IS_LOGIN = "IS_LOGIN";
 	public static final String LOGIN_USER = "LOGIN_USER";
 
-	public static final String TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+	public static final String  TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 	public static final Integer PROMO_NO_START = 1;
 	public static final Integer PROMO_STARTING = 2;
 	public static final Integer PROMO_END = 3;
